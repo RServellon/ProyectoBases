5625bd654448f6f0749eed9a4c42518f6a25b9e6
Héctor <hector.mendez.fernandez02@gmail.com>
2022-06-19 17:32:45 -0600

    update script principal

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,15 @@
--- Se configura el formato de la fecha
+-- Al correrlo, empezar desde donde se crean las secuencias,
+-- cuando da error YA NO sigue ejecutando el bloque que se selecciona
+
+-- Faltan datos de prueba de Archivo, Tarea-Archivo-Proyecto y RegistroHoraUsuario 
+
+-- Se borran las secuencias
+
 SET DATEFORMAT ymd
 DECLARE @dt DATETIME2 = '2016-01-02 12:03:28'
 SELECT @dt AS 'date from YMD Format'
 
--- Se borran las secuencias
+
 DROP SEQUENCE secRol;
 DROP SEQUENCE secUsuario;
 DROP SEQUENCE secPermiso;
@@ -28,6 +34,7 @@ DROP TABLE Proyecto;
 DROP TABLE Area;
 DROP TABLE Archivo;
 DROP TABLE Tarea;
+
 
 -- Se crean las secuencias
 CREATE SEQUENCE secRol 
@@ -138,6 +145,14 @@ CREATE TABLE Tarea (
 	costoReal DECIMAL(18,0) NOT NULL CHECK(costoReal >= 0)
 );
 
+INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, NULL, 'Planeacion', 18, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 180000.0, 0.0);
+INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Hacer cronograma de trabajo', 6, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 60000.0, 0.0);
+INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Oficio de inicio', 5, NULL, 'sin iniciar', 1,'2022-06-14 00:00:00', NULL, 50000.0, 0.0);
+INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Organizacion del equipo de trabajo', 7, NULL, 'completado', 2,'2022-06-14 00:00:00', NULL, 70000.0, 0.0);
+INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 3, 'Analisis de recursos tecnicos', 5, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 50000.0, 0.0);
+SELECT * FROM TAREA;
+DELETE FROM Tarea WHERE idpTarea = 6;
+
 CREATE TABLE Area (
 	idpArea INT NOT NULL PRIMARY KEY,
 	codigo VARCHAR(30) NOT NULL UNIQUE,
@@ -164,6 +179,12 @@ CREATE TABLE Archivo (
 	observacion TEXT NULL
 );
 
+-- No s� c�mo se suben los archivos
+-- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo, 'ACT-Pv5', 'Acta de constitucion', CAST('acta' AS VARBINARY(MAX)), NULL);
+-- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo);
+-- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo);
+-- SELECT * FROM Archivo;
+
 CREATE TABLE TareaArchivoProyecto (
 	idpTareaArchivoProyecto INT NOT NULL PRIMARY KEY, 
 	idfTarea INT NOT NULL FOREIGN KEY REFERENCES Tarea(idpTarea) ON DELETE CASCADE,
@@ -181,182 +202,6 @@ CREATE TABLE RegistroHoraUsuario (
 	observacion TEXT NULL
 );
 
--- Se crean los procedimientos INSERT para cada tabla
-GO
-CREATE PROCEDURE uspInsertPermiso (
-@pCrear INT,
-@pLeer INT, 
-@pBorrar INT,
-@pActualizar INT) 
-AS
-BEGIN
-INSERT INTO Permiso(idpPermiso,crear,leer,borrar,actualizar) 
-VALUES(NEXT VALUE FOR secPermiso, @pCrear,@pLeer , @pBorrar, @pActualizar);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertRol (
-@pCodigo VARCHAR(15), 
-@pNombre VARCHAR(30),
-@pPermiso INT)
-AS
-BEGIN
-INSERT INTO Rol(idpRol,codigo,nombre,permiso) 
-VALUES(NEXT VALUE FOR secRol,@pCodigo ,@pNombre ,@pPermiso);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertUsuario (
-@pIdentificacion VARCHAR(10), 
-@pNombre VARCHAR(30), 
-@pApellido VARCHAR(30), 
-@pCorreo VARCHAR(60),
-@pTelefono VARCHAR(20),
-@pcostoHora DECIMAL(18,0)) 
-AS
-BEGIN
-INSERT INTO Usuario(idpUsuario,identificacion,nombre,apellido,correo,telefono,costoHora) 
-VALUES(NEXT VALUE FOR secUsuario,@pIdentificacion ,@pNombre ,@pApellido,@pCorreo,@pTelefono, @pcostoHora);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertProyecto(
-@pCodigo VARCHAR(30), 
-@pNombre VARCHAR(60), 
-@pSiglas VARCHAR(10), 
-@pEstado VARCHAR(20),
-@pDescripcion TEXT, 
-@pfechaInicio DATETIME,
-@pfechaCierre DATETIME,
-@pCostoCalculado DECIMAL(18,0),
-@pCostoReal DECIMAL(18,0)) 
-AS
-BEGIN
-INSERT INTO Proyecto(idpProyecto,codigo,nombre,siglas,estado,descripcion,fechaInicio,fechaCierre,costoCalculado,costoReal)
-VALUES(NEXT VALUE FOR secProyecto,@pCodigo ,@pNombre ,@pSiglas,@pEstado,@pDescripcion,@pfechaInicio,@pfechaCierre,@pCostoCalculado,@pCostoReal );
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertRolUsuarioProyecto(
-@pIdUsuario VARCHAR(15), 
-@pIdfRol INT, 
-@pidfProyecto INT, 
-@pFechaAsignacion DATETIME,
-@pFechaDesasignacion DATETIME) 
-AS
-BEGIN
-INSERT INTO RolUsuarioProyecto(idpRolUsuarioProyecto,idUsuario,idfRol,idfProyecto,fechaAsignacion,fechaDesasignacion)
-VALUES(NEXT VALUE FOR secRolUsuarioProyecto,@pIdUsuario,@pIdfRol,@pidfProyecto,@pFechaAsignacion,@pFechaDesasignacion);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertTarea(
-@pIdfTarea  INT, 
-@pNombre VARCHAR(100), 
-@pDuracionHora INT,
-@pDescripcion TEXT,
-@pEstado VARCHAR(15),
-@pPrioridad INT,
-@pFechaAsignacion DATETIME,
-@pFechaCumplimiento DATETIME,
-@pCostoCalculado DECIMAL(18,0),
-@pCostoReal DECIMAL(18,0)) 
-AS
-BEGIN
-INSERT INTO Tarea(idpTarea,idfTarea,nombre,duracionHora,descripcion,estado,prioridad,fechaAsignacion,fechaCumplimiento,costoCalculado,costoReal)
-VALUES(NEXT VALUE FOR secTarea,@pIdfTarea,@pNombre,@pDuracionHora,@pDescripcion,@pEstado,@pPrioridad,@pFechaAsignacion,@pFechaCumplimiento,@pCostoCalculado,@pCostoReal);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertArea (
-@pCodigo VARCHAR(30), 
-@pNombre VARCHAR(30))
-AS
-BEGIN
-INSERT INTO Area(idpArea,codigo,nombre) 
-VALUES(NEXT VALUE FOR secArea ,@pCodigo ,@pNombre);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertProyectoArea (
-@pIdfProyecto INT, 
-@pIdfArea INT)
-AS
-BEGIN
-INSERT INTO ProyectoArea(idfProyecto,idfArea) 
-VALUES(@pIdfProyecto ,@pIdfArea);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertArchivo (
-@pCodigo VARCHAR(30), 
-@pNombre VARCHAR(50),
-@pArchivo VARBINARY(MAX),
-@pObservacion TEXT)
-AS
-BEGIN
-INSERT INTO Archivo(idpArchivo,codigo,nombre,archivo,observacion) 
-VALUES(NEXT VALUE FOR secArchivo, @pCodigo,@pNombre,@pArchivo,@pObservacion);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertTareaArchivoProyecto (
-@pIdfTarea INT, 
-@pIdfArchivo INT,
-@pidfProyecto INT)
-AS
-BEGIN
-INSERT INTO TareaArchivoProyecto(idpTareaArchivoProyecto,idfTarea,idfArchivo,idfProyecto) 
-VALUES(NEXT VALUE FOR secTareaArchivoProyecto,@pIdfTarea ,@pIdfArchivo,@pidfProyecto);
-END;
-GO
-
-GO
-CREATE PROCEDURE uspInsertRegistroHoraUsuario(
-@pIdfRolUsuarioProyecto INT, 
-@pIdfTareaArchivoProyecto INT,
-@pCantidadHoras INT,
-@pFechaRegistro DATETIME,
-@pObservacion TEXT)
-AS
-BEGIN
-INSERT INTO RegistroHoraUsuario(idpRegistroHoraUsuario,idfRolUsuarioProyecto,idfTareaArchivoProyecto,cantidadHoras,fechaRegistro,observacion) 
-VALUES(NEXT VALUE FOR secRegistroHoraUsuario,@pIdfRolUsuarioProyecto ,@pIdfTareaArchivoProyecto,@pCantidadHoras,@pFechaRegistro,@pObservacion);
-END;
-GO
-
--- PRUEBAS DE INSERTAR REGISTROS
-
-EXEC uspInsertPermiso 1,1,1,1;
-EXEC uspInsertPermiso 1,0,1,0;
-EXEC uspInsertPermiso 0,1,0,1;
-SELECT * FROM Permiso;
-
-
-EXEC uspInsertRol 'Admin', 'Administrador', 1;
-EXEC uspInsertRol 'Coor', 'Coordinador', 3;
-EXEC uspInsertRol 'Part', 'Participante', 2;
-SELECT * FROM Rol;
-
-EXEC uspInsertUsuario '100', 'Pedro', 'Gonzales', 'pedro@gmail.com', 11111111, 20000;
-EXEC uspInsertUsuario '101', 'Felipe', 'Campos', 'felipe@gmail.com', 22222222, 20000;
-EXEC uspInsertUsuario '102', 'Maria', 'Ramirez', 'maria@gmail.com', 33333333, 20000;
-EXEC uspInsertUsuario '103', 'Sara', 'Salas', 'sara@gmail.com', 44444444, 17000;
-EXEC uspInsertUsuario '104', 'Daniel', 'Villalobos', 'daniel@gmail.com', 55555555, 17000;
-EXEC uspInsertUsuario '105', 'Monica', 'Hernandez', 'monica@gmail.com', 66666666, 10000;
-EXEC uspInsertUsuario '106', 'Jose', 'Murillo', 'jose@gmail.com', 77777777, 10000;
-SELECT * FROM Usuario;
-
-/*
 
 INSERT INTO Permiso VALUES(NEXT VALUE FOR secPermiso, 1, 1, 1, 1);
 INSERT INTO Permiso VALUES(NEXT VALUE FOR secPermiso, 0, 1, 0, 0);
@@ -411,18 +256,5 @@ INSERT INTO ProyectoArea VALUES(1, 1);
 INSERT INTO ProyectoArea VALUES(3, 1);
 SELECT * FROM ProyectoArea;
 
-INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, NULL, 'Planeacion', 18, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 180000.0, 0.0);
-INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Hacer cronograma de trabajo', 6, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 60000.0, 0.0);
-INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Oficio de inicio', 5, NULL, 'sin iniciar', 1,'2022-06-14 00:00:00', NULL, 50000.0, 0.0);
-INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 1, 'Organizacion del equipo de trabajo', 7, NULL, 'completado', 2,'2022-06-14 00:00:00', NULL, 70000.0, 0.0);
-INSERT INTO Tarea VALUES(NEXT VALUE FOR secTarea, 3, 'Analisis de recursos tecnicos', 5, NULL, 'completado', 1,'2022-06-14 00:00:00', NULL, 50000.0, 0.0);
-SELECT * FROM TAREA;
-DELETE FROM Tarea WHERE idpTarea = 6;
 
--- No s� c�mo se suben los archivos
--- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo, 'ACT-Pv5', 'Acta de constitucion', CAST('acta' AS VARBINARY(MAX)), NULL);
--- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo);
--- INSERT INTO Archivo VALUES(NEXT VALUE FOR secArchivo);
--- SELECT * FROM Archivo;
 
-*/
